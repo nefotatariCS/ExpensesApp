14611fc915c665ad8392f139983439b69d6192a0
RedianHoxha <51862189+RedianHoxha@users.noreply.github.com>
2024-08-16 17:18:05 +0200

    Added Unit Tests FE and BE

--- expenses-fe/src/app/pages/user/user.component.spec.ts
+++ expenses-fe/src/app/pages/user/user.component.spec.ts
@@ -4,7 +4,9 @@ import { UserService } from '../../_services/user.service';
 import { UserRoleService } from '../../_services/userRole.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { TableModule } from 'primeng/table';
+import { of } from 'rxjs';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -26,6 +28,25 @@ describe('UserComponent', () => {
       return null;
     });
 
+    // Mock responses from services
+    userRoleServiceMock.getAllUserRoles.and.returnValue(
+      Promise.resolve({
+        userRoles: [
+          { id: 1, userRoleDescription: 'Admin' },
+          { id: 2, userRoleDescription: 'User' },
+        ],
+      })
+    );
+
+    userServiceMock.getAllUsers.and.returnValue(
+      Promise.resolve({
+        users: [
+          { id: 1, userName: 'john', userRoleId: 1 },
+          { id: 2, userName: 'jane', userRoleId: 2 },
+        ],
+      })
+    );
+
     await TestBed.configureTestingModule({
       declarations: [UserComponent],
       imports: [HttpClientModule, RouterTestingModule, TableModule],
@@ -37,9 +58,24 @@ describe('UserComponent', () => {
 
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch and set all user roles on initialization', async () => {
+    await component.ngOnInit();
+    expect(userRoleServiceMock.getAllUserRoles).toHaveBeenCalledWith(
+      'mockAccessToken'
+    );
+    expect(component.allUserRoles.length).toBe(2); // Check if user roles were set correctly
+  });
+
+  it('should fetch and set all users on initialization', async () => {
+    await component.ngOnInit();
+    expect(userServiceMock.getAllUsers).toHaveBeenCalledWith('mockAccessToken');
+    expect(component.allUsers.length).toBe(2); // Check if users were set correctly
   });
 });
