e0ab68f831775366a36d4e43aab7de3ea1a657d7
RedianHoxha <51862189+RedianHoxha@users.noreply.github.com>
2024-10-03 11:45:48 +0200

    connect to db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "P0kuj3m3r",
-  DB: "ExpensesApp",
+  HOST: "clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
+  USER: "uciaj6hg079haf",
+  PASSWORD: "pb0a2daafddc5f5190e2938dbe015b45c432b6b5f7ede431497b8c05060e67a07",
+  DB: "dbna93utrlpmlk",
   dialect: "postgres",
   pool: {
     max: 5,
